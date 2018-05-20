7d3ed9549dcf2507d29de0cd835435524978af20
AravindSarathChandraKumar <aravinds95@gmail.com>
2018-05-20 16:30:42 +0530

    change main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,81 @@
-
-document.getElementById('myform').addEventListener('submit',saveBookmark);
-function saveBookmark(e){
+document.getElementById('myform').addEventListener('submit', saveBookmark);
+function saveBookmark(e)
+{
     var siteName = document.getElementById('siteName').value;
-    var siteURL = document.getElementById('siteURL').value;
-    var bookmark={name:siteName,value:siteURL}
+    var siteURL = document.getElementById('siteUrl').value;
+    if(!validateForm(siteName,siteURL)){
+        return false;
+    }
+    var bookmark = {name:siteName,
+                    url:siteURL
+                   }
     
     
     if(localStorage.getItem('bookmarks')===null){
-        var bookmarks=[];
+        var bookmarks = [];
         bookmarks.push(bookmark);
         localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
     }else{
-        
         var bookmarks=JSON.parse(localStorage.getItem('bookmarks'));
         bookmarks.push(bookmark);
-        localStorage.setItem('bookmarks',JSON.stringify('bookmark'));
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    }
+        document.getElementById('myform').reset();
+        
+        fetchBookmarks();
+        e.preventDefault();
+    
+    
+}
+
+function deleteBookmark(url){
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for(var i=0;i<bookmarks.length;i++){
+        
+    if(bookmarks[i].url==url){
+        
+        bookmarks.splice(i,1);
+    
+       }
+    }
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    
+    fetchBookmarks();
+}
+
+function fetchBookmarks(){
+
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  var bookmarksResults = document.getElementById('results');
+  bookmarksResults.innerHTML = '';
+  for(var i = 0; i < bookmarks.length; i++){
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+
+    bookmarksResults.innerHTML += '<div class="card bg-light text-dark card-body">'+
+                                  '<h3>'+name+
+                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+                                  '</h3>'+
+                                  '</div>';
+  }
+}
+
+function validateForm(siteName,siteURL){
+    if(!siteName || !siteURL){
+        
+        alert("Please fill the form");
+        return false;
         
     }
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
     
-    e.preventDefault();
+    if(!siteURL.match(regex)){
+        
+        alert("Please use a valid URL");
+        return false;
+    }
+    return true;
     
 }
